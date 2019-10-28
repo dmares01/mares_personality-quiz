1ce4dc30bc8fa014f259ee52153fc3925ab22d15
Drew Mares <dmares01@hamline.edu>
2019-10-28 10:58:17 -0500

    Initial Commit

---
+++ Mares_Personality_Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mares_Personality_Quiz
+//
+//  Created by Drew Mares on 10/28/19.
+//  Copyright © 2019 Drew Mares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
